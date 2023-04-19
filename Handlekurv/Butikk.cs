20a58b60cf58b7623ffe79a5b5f86f119bf46c23
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-19 14:19:19 +0200

    r

--- Handlekurv/Butikk.cs
+++ Handlekurv/Butikk.cs
@@ -13,6 +13,21 @@
             var sverd = new Produkt("Sverd", 500);
             var duftlys = new Produkt("Duftlys", 400);
             var cosplay = new Produkt("Cosplay", 30000);
+            var handlekurv = new Handlekurv();
+            handlekurv.visKurv(handlekurv);
+            buy(handlekurv, fursuit);
+            buy(handlekurv, duftlys);
+            handlekurv.visKurv(handlekurv);
+
+        }
+        public static void buy(Handlekurv handleKurv, Produkt product, int count)
+        {
+
+        }
+        public static void buy(Handlekurv handleKurv, Produkt product)
+        {
+            handleKurv.Produktliste.Add(product);
+            Console.WriteLine($"Du har kjøpt 1 {product.Navn} til {product.Pris}kromer!");
         }
     }
 }
