28e47be8479860ef9a0df1e280755af5e34478a6
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-19 13:19:10 +0200

    e

---
+++ Handlekurv/Butikk.cs
@@ -0,0 +1,18 @@
+﻿namespace Handlekurv
+{
+    public class Butikk
+    {
+        public string Name { get; private set; }
+        public Butikk()
+        {
+            Name = "Erwans nichebutikk";
+        }
+        public void runShop()
+        {
+            var fursuit = new Produkt("Fursuit", 36000);
+            var sverd = new Produkt("Sverd", 500);
+            var duftlys = new Produkt("Duftlys", 400);
+            var cosplay = new Produkt("Cosplay", 30000);
+        }
+    }
+}
