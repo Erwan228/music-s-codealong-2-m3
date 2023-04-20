555cfb5b3500ff60bf8dc4c8d10312e50554b67b
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-04-20 11:12:32 +0200

    h

--- Handlekurv/Orderline.cs
+++ Handlekurv/Orderline.cs
@@ -2,5 +2,10 @@
 {
     internal class Orderline
     {
+        public Produkt Produkt { get; private set; }
+        public int Antall { get; private set; }
+        public Orderline()
+        {
+        }
     }
 }
